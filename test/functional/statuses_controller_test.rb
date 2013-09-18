8881c5fee5be827f8760be481dccdea66d003354
David Gross <dgross881@gmail.com>
2013-09-18 09:52:47 +0800

    updated status controller user

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -39,6 +39,17 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
   
+  test "should create status for current users when logged in" do
+      sign_in users(:david) 
+
+     assert_difference('Status.count') do
+     post :create, status: { content: @status.content, user_id: users(:joe).id}
+   end
+     
+     assert_redirected_to status_path(assigns(:status))
+     assert_equal assigns(:status).user_id, users(:david).id
+   end
+  
   test "should show status when logged in" do
     get :show, id: @status
     assert_response :success
@@ -63,7 +74,27 @@ class StatusesControllerTest < ActionController::TestCase
     put :update, id: @status, status: { content: @status.content}
     assert_redirected_to status_path(assigns(:status))
   end
+  
+  test "should update status for current users when logged in" do
+    sign_in users(:david) 
+    
+    put :update, id: @status, status: { content: @status.content, user_id: users(:joe).id }  
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:david).id
+  end
+  
+  
+  
+  test "should not update the status if nothing has changed" do
+    sign_in users(:david)  
+    put :update, id: @status  
+    
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:david).id
+  end
 
+  
+  
   test "should destroy status" do
     assert_difference('Status.count', -1) do
       delete :destroy, id: @status
