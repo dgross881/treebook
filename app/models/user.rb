84b4fc9b54a18f4d8d2897614cbe61e77816657d
David Gross <dgross881@gmail.com>
2013-09-18 09:57:22 +0800

    Changed the user sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,11 +15,11 @@ class User < ActiveRecord::Base
   
   validates :last_name, presence: true
 
-  validates :profile_name, presence: true,
-                             uniqueness: true,
-                             format: {
-                             with: /^[a-zA-Z0-9_-]+$/,
-                             message: 'Must be formatted correctly.'
+  validates :profile_name, presence: true, 
+                           uniqueness: true,
+                            format: {
+                            with: /^[a-zA-Z0-9_-]+$/,
+                            message: 'Must be formatted correctly.'
                              }
                           
   validates :email, presence: true
